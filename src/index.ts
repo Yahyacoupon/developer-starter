b801ddf222e828efb2bd6db3a9c7e27e2fa05f85
YahyaCoupon <amyahyacoup@gmail.com>
2025-08-31 22:50:40 +0300

    Changed no to #36

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Next Iteration test #34';
+  const name = 'Next Iteration test #36';
   greetUser(name);
 });
