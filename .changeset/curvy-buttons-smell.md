a49b1e78a1d66ee6c3b3f75a0b1082e30cb4cff5
YahyaCoupon <amyahyacoup@gmail.com>
2025-08-31 23:20:35 +0300

    Change no to #100

---
+++ .changeset/curvy-buttons-smell.md
@@ -0,0 +1,5 @@
+---
+'dev-starter-webflow': minor
+---
+
+Change number to #100
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Next Iteration test #38';
+  const name = 'Next Iteration test #100';
   greetUser(name);
 });
